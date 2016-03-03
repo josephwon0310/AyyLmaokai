3fec10284e90766510558692271efb0ee76a586a
Chukwudi Derek Uche <cuche@purdue.edu>
2016-03-03 01:11:37 -0500

    Created initial design for feedback

---
+++ app/views/feedback.py
@@ -0,0 +1,30 @@
+from flask import Blueprint, render_template
+from app import app
+
+feedback = Blueprint('feedback', __name__)
+
+#Teams
+OUR_TEAM = "OURS"
+THEIR_TEAM = "THEIRS"
+
+class Game:
+    self.out_team = []
+    self.their_team = []
+    self.did_win = True
+
+class Players(champion, player_name, team):
+    self.name = player_name
+    self.team =  team
+    self.champion = champion
+
+@app.route('/feedback')
+def create_feedback():
+    #get list of last games and player names
+
+    #
+
+    '''
+        return them in class structure above
+    '''
+
+    return render_template('/feedback/feedback.html', list_obj=list_obj)
