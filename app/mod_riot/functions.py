558b7887c0219ea03f4a36d7a6c1deff2bbcb9dc
josephwon0310 <won20@purdue.edu>
2016-03-03 04:56:58 -0500

    requirements.txt

--- app/mod_riot/functions.py
+++ app/mod_riot/functions.py
@@ -11,6 +11,8 @@ SUMMONER_URL = 'https://na.api.pvp.net/api/lol/na/v1.4/summoner'
 STATS_URL = 'https://na.api.pvp.net/api/lol/na/v1.3/stats/by-summoner'
 LEAGUE_URL = 'https://na.api.pvp.net/api/lol/na/v2.5/league'
 GAME_URL = 'https://na.api.pvp.net/api/lol/na/v1.3/game/by-summoner'
+CURRENT_MATCH = 'https://na.api.pvp.net/observer-mode/rest/consumer/getSpectatorGameInfo/NA1'
+
 CHAMPIONGG = 'http://api.champion.gg/stats'
 
 #returns the json formatted summary of the requested summoner.
@@ -53,14 +55,17 @@ def get_ranked_stats(sum_ID, season, API_KEY):
     elif r.status_code == 404: #ranked stats not found
         return 404
 
-def get_general_stats(sum_ID, API_KEY, season):
+#Returns the json info of the current game that the user is in
+def get_current_match(sum_ID, API_KEY):
     
-    url = ''
+    url = '{}/{}?api_key={}'.format(CURRENT_MATCH, sum_ID, API_KEY)
     r = requests.get(url)
     
     if r.status_code == 200:
-        return r.json()
-    elif r.status_code == 404: #stats not found, most likely wrong summoner_ID
+        data = r.json()
+        players = data['participants'] #gets the list of json objects of the players
+        
+    elif r.status_code == 404: #not in game
         return 404
 
 #returns the json object of the challenger tier
--- app/views/home.py
+++ app/views/home.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template
 from flask import request
 from flask import current_app
 from app.mod_riot import functions as f
-from app.mod_riot import mapper as m
+from string import whitespace
 import requests
 
 home = Blueprint('home', __name__)
@@ -11,14 +11,20 @@ home = Blueprint('home', __name__)
 def index():
     return render_template('home/home.html')
 
+@home.route('/test')
+def test():
+    return "hello world"
+
 @home.route('/', methods=['POST'])
 def riot_request():
 
     API_KEY = current_app.config.get('RIOT_API_KEY') #api_key for riotgames
     
     summonerName = request.form['text']
+    sumName = str(summonerName)
+    sumName.replace(" ", "")#strip whitespace
     summonerDTO = f.get_sum_DTO(summonerName, API_KEY)
-    summonerDTO = summonerDTO[summonerName]
+    summonerDTO = summonerDTO[sumName]
     
     #retrieve individual values from summonerDTO
     summonerId = summonerDTO['id']
