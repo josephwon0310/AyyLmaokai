28fd8d48d3bab8d8a3f66ad424620f8e88d917b9
Chukwudi Derek Uche <cuche@purdue.edu>
2016-03-04 17:33:45 -0500

    Fixed trending. What happened to navbar?

--- app/forms.py
+++ app/forms.py
@@ -5,7 +5,6 @@ from wtforms.validators import DataRequired, EqualTo, Required
 class SummonerForm(Form):
     summonerName = StringField('summonerName', validators=[DataRequired()])
 
-<<<<<<< Updated upstream
 class GameBans(Form):
 	ban1 = StringField('ban1', validators=[DataRequired()])
 	ban2 = StringField('ban2', validators=[DataRequired()])
@@ -13,9 +12,7 @@ class GameBans(Form):
 	ban4 = StringField('ban4', validators=[DataRequired()])
 	ban5 = StringField('ban5', validators=[DataRequired()])
 	ban6 = StringField('ban6', validators=[DataRequired()])
-	
-=======
+
 class SignUpForm(Form):
     username = StringField('Summoner Name', [Required()])
     confirm = PasswordField('Confimation')
->>>>>>> Stashed changes
