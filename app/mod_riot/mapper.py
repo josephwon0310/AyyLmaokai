b73ee3b69cafefd9167c30db008ed7434397b362
josephwon0310 <won20@purdue.edu>
2016-03-03 19:39:43 -0500

    added champion images to static

--- app/mod_riot/mapper.py
+++ app/mod_riot/mapper.py
@@ -4,26 +4,34 @@ Maps the data fetched from Riot API to the static data in dragontail
 
 import json
 import os
-import requests
+import requests, shutil
 
 from pprint import pprint
 
 REALM = 'http://ddragon.leagueoflegends.com/realms/na.json'
-DRAGON_TAIL_CHAMPION = 'http://ddragon.leagueoflegends.com/cdn/6.4.2/data/en_US/champion.json'
-CHAMPION_IMAGE = 'app/mod_riot/dragontail-6.4.1/6.4.1/img/champion/'
+DRAGON_TAIL_CHAMPION = 'http://ddragon.leagueoflegends.com/cdn/{}/data/en_US/champion.json'
+CHAMPION_IMAGE = 'http://ddragon.leagueoflegends.com/cdn/{}/img/champion/{}'
 
 #returns the version of the Realm file
-def check_datadragon_version(attribute):
+def check_datadragon_version():
     
     r = requests.get(REALM)
     data = r.json()
-    data = data['n']
+    version = data['v']
     
-    return data[attribute]
+    if r.status_code == 200:
+        return version
+        
+    else:
+        return "SOMETHING IS WITH DATA REALM!"
 
+#maps the champion id to its name
 def map_champions(id):
     
-    r = requests.get(DRAGON_TAIL_CHAMPION)
+    version = check_datadragon_version()
+    url = DRAGON_TAIL_CHAMPION.format(version)
+    
+    r = requests.get(url)
     data = r.json()
     
     #Iterate through the champion json file
@@ -34,9 +42,18 @@ def map_champions(id):
             #TODO: currently only returns the name
 
 
-#returns the file, not sure if it's the best practice
+#returns the response object of the image
 def get_splash_art(champion):
-    #joseph will rewrite
-
+    
+    version = check_datadragon_version()
+    url = CHAMPION_IMAGE.format(version, champion)
+    #url = 'http://ddragon.leagueoflegends.com/cdn/6.4.2/img/champion/Aatrox.png'
+    r = requests.get(url, stream=True)
+    
+    if r.status_code == 200:
+        return r
+    else:
+        return "RIP"
+        
 def map_items(id):
     pass
--- app/views/home.py
+++ app/views/home.py
@@ -1,9 +1,12 @@
 from flask import Blueprint, render_template, redirect, url_for
-from flask import request
+from flask import request, make_response
 from flask import current_app
+
 from app.mod_riot import functions as f
+from app.mod_riot import mapper as m
+
 import requests
-from app.forms import *
+
 
 home = Blueprint('home', __name__)
 
@@ -21,6 +24,7 @@ def index():
     return render_template('home/home.html')
 
 
+#-=======TESTING
 ##Example code on how to use ChampionGG data
 @home.route('/testChamp')
 def test():
@@ -35,6 +39,24 @@ def test():
     cs = data[0]['minionsKilled']
     
     return render_template('fck.html', winPercent=winPercent)
+
+
+#EXAMPLE OF HOW YOU WOULD RETURN IMAGE FROM REQUEST VIA FLASK
+@home.route('/image')
+def imagetest():
+
+    #Currently harcoded to return Nami's image
+    champion = "Nami.png"
+    
+    #this is the response object 
+    image = m.get_splash_art(champion)
+    
+    wah = make_response(image.content)
+    wah.headers['Content-Type'] = 'image/png'
+    
+    return wah
+
+#======TESTING
 
 @home.route('/', methods=['POST'])
 def riot_request():
