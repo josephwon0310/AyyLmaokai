e4d2459208ccfe0c3e46fae3857fa7f6c213889d
Chukwudi Derek Uche <uchederek@yahoo.com>
2016-04-08 09:27:46 -0400

    fixed bugs

--- app/views/signup.py
+++ app/views/signup.py
@@ -12,20 +12,15 @@ signup = Blueprint('signup', __name__)
 def sign_up():
     form = forms.SignUpForm()
     if request.method == 'POST':
-        if (form.password.data != form.confirm.data):
-            return
+        if (form.password.data == form.confirm.data):
+            #put a random division here because too lazy to
+            #actualy get real division
+            u = models.User(league_name = form.summoner_name.data,
+                            password = form.password.data,
+                            email = form.email.data,
+                            rank = 6)
 
-        print "Email: " + form.email.data
-        print "Password: " + form.password.data
-
-        #put a random division here because too lazy to
-        #actualy get real division
-        u = models.User(league_name = form.summoner_name.data,
-                        password = form.password.data,
-                        email = form.password.data,
-                        rank = 6)
-
-        db.session.add(u)
-        db.session.commit()
+                db.session.add(u)
+                db.session.commit()
         return redirect('/login')
     return render_template('signup/signup.html',  title='Sign Up', form=form)
