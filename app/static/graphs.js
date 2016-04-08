15637bc4cf3410c7611dc779318d4b82ceb72165
josephwon0310 <won20@purdue.edu>
2016-04-08 04:39:29 -0400

    graph js

---
+++ app/static/graphs.js
@@ -0,0 +1,21 @@
+//recent 10 games win rate graph
+
+var winRateData = [
+    {
+        value: winrate,
+        highlight: '#1A1AFF',
+        color: '#4747FF',
+        label: 'Won'
+    },
+    {
+        value: 10-winrate,
+        color: '#ff4e4e',
+        highlight: '#ff3434',
+        label: 'Lost'
+    }
+]
+
+var rwinrateCtx = $("#recentWinrate").get(0).getContext("2d");
+var rwinrateChart = new Chart(rwinrateCtx).Pie(winRateData, {
+    responsive: true
+});
