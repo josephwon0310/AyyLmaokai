acee33dbcc60e2d85e3d2c598c7b493f5e31cf84
byronleecs <lord.byronlee@gmail.com>
2016-04-09 16:52:03 -0400

    bloop

--- app/views/signup.py
+++ app/views/signup.py
@@ -12,21 +12,6 @@ signup = Blueprint('signup', __name__)
 def sign_up():
     form = forms.SignUpForm()
     if request.method == 'POST':
-<<<<<<< HEAD
-
-        print "Email: " + form.email.data
-        print "Password: " + form.password.data
-
-        #put a random division here because too lazy to
-        #actualy get real division
-        u = models.User(league_name = form.summoner_name.data,
-                        password = form.password.data,
-                        email = form.email.data,
-                        rank = 6)
-
-        db.session.add(u)
-        db.session.commit()
-=======
         if (form.password.data == form.confirm.data):
             #put a random division here because too lazy to
             #actualy get real division
@@ -35,8 +20,7 @@ def sign_up():
                             email = form.email.data,
                             rank = 6)
 
-                db.session.add(u)
-                db.session.commit()
->>>>>>> 964e9fef1c401acc892ddb11424a975ef3aa48dd
+            db.session.add(u)
+            db.session.commit()
         return redirect('/login')
     return render_template('signup/signup.html',  title='Sign Up', form=form)
