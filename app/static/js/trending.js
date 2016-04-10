4f6ee48a0b9c5b5e163fab6b6b25e89af9f206e2
josephwon0310 <won20@purdue.edu>
2016-04-09 23:22:16 -0400

    more graphs

--- app/static/js/trending.js
+++ app/static/js/trending.js
@@ -2,11 +2,20 @@ var mostPlayedData = {
     labels: ["Lucian", "Ezreal", "Thresh", "Vayne", "Lee Sin", "Jhin", "Janna", "Braum", "Alista", "Blitzcrank"],
     datasets: [
         {
+            label: "playrate",
             fillColor: "rgba(220,220,220,0.5)",
             strokeColor: "rgba(220,220,220,0.8)",
             highlightFill: "rgba(220,220,220,0.75)",
             highlightStroke: "rgba(220,220,220,1)",
             data: [37.3, 28.0, 27.0, 23.0, 21.4, 20.5, 17.4, 16.7, 14.9, 14.4]
+        },
+        {
+            label: "winrate",
+            fillColor: "rgba(151,187,205,0.5)",
+            strokeColor: "rgba(151,187,205,0.8)",
+            highlightFill: "rgba(151,187,205,0.75)",
+            highlightStroke: "rgba(151,187,205,1)",
+            data: [49.4, 50.8, 48.3, 50.1, 46.2, 52.0, 53.4, 51.3, 49.3, 52.7]
         }
     ]
     
